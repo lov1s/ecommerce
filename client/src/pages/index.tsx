668eaa18d20bd21c165a3dcb2b5e060a2b771f0f
lov1s <artem.boss619@gmail.com>
2023-05-02 14:05:25 +0300

    client commit

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Home from './home/Home'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function HomePage() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <Home></Home>
+    </main>
+  )
+}
